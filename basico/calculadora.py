0da96fd6feab523be39b04710c0806db37f7810d
Victor Manoel <victolopes2002@gmail.com>
2025-07-14 09:54:07 -0300

    Create calculadora.py

---
+++ basico/calculadora.py
@@ -0,0 +1,9 @@
+num1 =  0
+op =    ''
+num2 =   0
+result = 0
+
+while True:
+    num1 = float(input("Digite o primeiro número: "))
+    op = int(input("Qual será a operação: "))
+    num2 = float(input("Digite o segundo número: "))
